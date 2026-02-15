de7ad2f072d7ef1b3f257eeab8f530a248b9782f
Nils Pukropp <nils@narl.io>
2026-02-15 21:44:39 +0100

    fixed host configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const allowedHosts = env.VITE_ALLOWED_HOSTS?.split(',') || [];
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true, // Allow external access
+      allowedHosts: allowedHosts,
+    }
+  }
 })
