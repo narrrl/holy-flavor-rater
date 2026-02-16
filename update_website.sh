228149c0d8f6b37eb75cb1034e51e640fa1ce76d
Nils Pukropp <nils@narl.io>
2026-02-16 17:20:10 +0100

    updated docker compose and update_website

--- update_website.sh
+++ update_website.sh
@@ -1,6 +1,10 @@
 sudo docker compose down
-git pull origin main
+git stash
+git checkout main
+git pull
+git checkout narl.io
 git merge main
+git stash apply
 CONFLICTS=$(git ls-files -u | wc -l)
 if [ "$CONFLICTS" -gt 0 ] ; then
    echo "There are merge conflicts, resolve them and run `sudo docker compose up -d --build` or abort with `git merge --abort`"
